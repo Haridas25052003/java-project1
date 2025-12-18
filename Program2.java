2420075d290a425321c3539a34c82715fcbab7e9
Haridas250503 <haridasshinde314@gmail.com>
2025-12-19 00:04:11 +0530

    add above file

---
+++ Program2.java
@@ -0,0 +1,27 @@
+/*
+*
+**
+***
+****
+*****
+*/
+
+class Program2
+{
+public static void main(String[]args)
+{
+
+       int n=5;
+
+       for(int i=1;i<=5;i++){ //i=3
+
+           for(int j=i;j>=1;j--){//j=2 >=1
+
+               System.out.print("*");
+
+           }
+
+           System.out.println();
+       }
+ }
+}
