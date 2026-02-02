96447cb6281485eb47a34446638f3bc5abbd69be
Haridas250503 <haridasshinde314@gmail.com>
2026-02-02 23:54:16 +0530

    covered advanced array programs

---
+++ MinElement.java
@@ -0,0 +1,21 @@
+class MinElement
+{
+public static void main(String[]args)
+{
+
+  int a[]={3,7,2,9,1};
+  
+  int min=a[0];
+
+  for(int i=0;i<a.length();i++){
+
+      if(a[i] < min){
+
+        min=a[i];
+ 
+      }
+   }
+
+   System.out.println(min);
+ }
+}
---
+++ Program163.java
@@ -0,0 +1,22 @@
+/*
+         *
+        * *
+      *  *  *
+    *  *  *  *
+  *  *  *  *   *
+*/
+class Program163
+{
+    public static void main(String[]args){
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=i;k>=1;k--){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}   
