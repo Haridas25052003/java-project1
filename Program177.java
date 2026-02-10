2ff99f9b6173825ba0c6d79f5a3a4dcb07c891fa
Haridas250503 <haridasshinde314@gmail.com>
2026-02-10 23:58:27 +0530

    add

---
+++ Program177.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+public class Program177 {
+    public static void main(String[] args) {
+        
+        int a[]={5,1,4,2};
+        for(int i=0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]>a[j]){
+                    int temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
